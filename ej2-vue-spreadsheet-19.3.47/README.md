c088ff68a9952d0257876c148ab9e31944be9de3
Kevin Chisholm <kevinchisholm@MacBooks-MacBook-Pro.local>
2021-10-28 13:24:24 -0400

    Add ej2-vue-spreadsheet-19.3.47

---
+++ ej2-vue-spreadsheet-19.3.47/README.md
@@ -0,0 +1,9 @@
+# syncfusion-incidents
+Demo Code for ej2-vue-spreadsheet-19.2.59
+
+## Local Setup
+
+```
+yarn install
+yarn serve
+```
---
+++ ej2-vue-spreadsheet-19.3.47/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+@import "../node_modules/@syncfusion/ej2-vue-spreadsheet/styles/material.css";
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';  
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';  
+@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';  
+@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';  
+@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-grids/styles/material.css';
+@import "../node_modules/@syncfusion/ej2-spreadsheet/styles/material.css";
+</style>
