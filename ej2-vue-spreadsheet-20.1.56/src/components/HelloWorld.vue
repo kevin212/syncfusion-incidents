af1655e4a6316bf31fd375ab0dff26cc2d05a9a8
Kevin Chisholm <kevin212@pobox.com>
2022-05-18 05:55:23 -0400

    Upgrade to 20.1.56

--- ej2-vue-spreadsheet-20.1.56/src/components/HelloWorld.vue
+++ ej2-vue-spreadsheet-20.1.56/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="control-section">
     <div id="spreadsheet-default-section">
-      <div><h1>@syncfusion/ej2-vue-spreadsheet VER 20.1.55</h1></div>
+      <div><h1>@syncfusion/ej2-vue-spreadsheet VER 20.1.56</h1></div>
       <div class="spreadsheet-buttons-container">
         <button
         cssClass="e-control"
